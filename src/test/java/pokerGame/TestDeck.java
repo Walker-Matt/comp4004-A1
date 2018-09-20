1c420595d09c21eddd13b4d7124c0d8a5b73efe8
Walker <walker_matt@hotmail.ca>
2018-09-19 23:02:18 -0400

    (Refactor) Refactored TestDeck
    Fixed issue in TestDecks testDeckGetDeck method, where I made a mistake with the logic, now it assures that it returns a List object.

--- src/test/java/pokerGame/TestDeck.java
+++ src/test/java/pokerGame/TestDeck.java
@@ -11,9 +11,6 @@ public class TestDeck extends TestCase {
 	//test getter for a Decks list of Cards
 	public void testDeckGetDeck() {
 		Deck d = new Deck();
-		List<Card> cards = new ArrayList<Card>();
-		Card card = new Card();
-		cards.add(card);
-		assertEquals(card, d.getDeck());
+		assertTrue(d.getDeck() instanceof List<?>);
 	}
 }
