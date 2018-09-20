d8b5e8a70f9900ea7a2bead555ca7ed58f0e0baf
Walker <walker_matt@hotmail.ca>
2018-09-20 10:18:51 -0400

    (Code) Added getSize() to Deck
    Added getSize() method to Deck which returns the size of the list of Cards, as well as created a proper Deck constructor which instantiates the cards List, and read in the cards.txt file, parses each line, and adds a Card object to the cards List for each line.

--- src/main/java/pokerGame/Deck.java
+++ src/main/java/pokerGame/Deck.java
@@ -1,12 +1,33 @@
 package pokerGame;
 
+import java.io.*;
 import java.util.*;
 
 public class Deck {
 	protected List<Card> cards;
 	
-	public List<Card> getDeck() {
+	public Deck() {
 		cards = new ArrayList<Card>();
-		return cards;
+		
+		String fileName = "cards.txt";
+		String line = null;
+		
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while((line = bufferedReader.readLine()) != null) {
+                this.cards.add(new Card());
+            }
+            bufferedReader.close();
+		}
+		catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");               
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
 	}
+	
+	public List<Card> getDeck() { return cards; }
+	public int getSize() { return cards.size(); }
 }
