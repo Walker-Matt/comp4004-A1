c476f24b10c8a413673342789b55142df02f2680
Walker <walker_matt@hotmail.ca>
2018-09-22 14:19:11 -0400

    (Refactor) Fixed TestHands
    Learned my lesson by accidentally naming a test with Test at the start, rather than test (has to be lowercase...).

--- src/test/java/pokerGame/TestHands.java
+++ src/test/java/pokerGame/TestHands.java
@@ -1,7 +1,7 @@
 package pokerGame;
 
-import java.util.*;
 import junit.framework.*;
+import java.util.*;
 
 public class TestHands extends TestCase {
 	public TestHands(String name) {
@@ -9,13 +9,13 @@ public class TestHands extends TestCase {
 	}
 	
 	//test check for a Royal Flush
-	public void TestHandRoyalFlush() {
+	public void testHandRoyalFlush() {
 		List<Card> royalFlush = new ArrayList<Card>();
 		royalFlush.add(new Card("H", "10"));
 		royalFlush.add(new Card("H", "J"));
 		royalFlush.add(new Card("H", "Q"));
 		royalFlush.add(new Card("H", "K"));
 		royalFlush.add(new Card("H", "A"));
-		assertTrue(Hand.isRoyalFlush(royalFlush));
+		assertTrue(Hands.isRoyalFlush(royalFlush));
 	}
 }
