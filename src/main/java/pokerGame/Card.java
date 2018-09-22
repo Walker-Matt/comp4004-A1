72296f6598b6008bcf1d4507c709c2b4c75a2369
Walker <walker_matt@hotmail.ca>
2018-09-22 14:38:19 -0400

    (Code) Added order to Card
    Added order attribute to the Card class, as well as a proper way of setting the order, and a getter: getOrder().

--- src/main/java/pokerGame/Card.java
+++ src/main/java/pokerGame/Card.java
@@ -3,15 +3,30 @@ package pokerGame;
 public class Card {
 	protected String suit;
 	protected String rank;
+	protected int order;
 	
 	public Card(String suit, String rank) {
 		this.suit = suit;
 		this.rank = rank;
+		
+		if(rank.equals("J")) {
+			order = 10;
+		} else if(rank.equals("Q")) {
+			order = 11;
+		} else if(rank.equals("K")) {
+			order = 12;
+		} else if(rank.equals("A")) {
+			order = 13;
+		} else {
+			order = Integer.parseInt(rank) - 1;
+		}
 	}
 	
 	public String getSuit() { return suit; }
 
 	public String getRank() { return rank; }
+	
+	public int getOrder() { return order; }
 	
 	public String toString() { return suit + rank; }
 }
