dc1a4c79cd13b81cdc6888f0054142e0f2bbb8c9
Walker <walker_matt@hotmail.ca>
2018-09-22 08:36:48 -0400

    (Test) Added test to TestGame
    Added a test to TestGame which tests the display Strings used to output the state of the game to the console.

--- src/test/java/pokerGame/TestGame.java
+++ src/test/java/pokerGame/TestGame.java
@@ -10,7 +10,6 @@ public class TestGame extends TestCase {
 	
 	//test the initial game setup values
 	public void testGameSetup() {
-		Game game = new Game();
 		String[] args = new String[0];
 		Game.main(args);
 		
@@ -29,5 +28,23 @@ public class TestGame extends TestCase {
 		assertEquals(2, Game.AIPcards.size());
 		
 		assertEquals(45, Game.deck.getSize());
+	}
+	
+	//test String values used for display
+	public void testGameDisplay() {
+		String[] args = new String[0];
+		Game.main(args);
+		
+		String river = "RIVER: " + Game.river.get(0).getSuit() + Game.river.get(0).getRank() + " " +
+				Game.river.get(1).getSuit() + Game.river.get(1).getRank() + " " +
+				Game.river.get(2).getSuit() + Game.river.get(2).getRank() + "\n";
+		String cardsToBeat = "Cards to beat: " + Game.cardsToBeat.get(0).getSuit() + Game.cardsToBeat.get(0).getRank() +
+				" " + Game.cardsToBeat.get(1).getSuit() + Game.cardsToBeat.get(1).getRank() + "\n";
+		String AIPcards = "AIP cards: " + Game.AIPcards.get(0).getSuit() + Game.AIPcards.get(0).getRank() + " " +
+				Game.AIPcards.get(1).getSuit() + Game.AIPcards.get(1).getRank() + "\n";
+		
+		AssertEquals(river, Game.riverDisplay);
+		AssertEquals(cardsToBeat, Game.cardsToBeatDisplay);
+		AssertEquals(AIPcards, Game.AIPcardsDisplay);
 	}
 }
