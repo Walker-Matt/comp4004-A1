972f9c76d441f0e4e2a7593bdcfc8106620ed265
Walker <walker_matt@hotmail.ca>
2018-09-22 19:41:06 -0400

    (Code) Added Two Pair check to Hands
    Added a method to Hands that takes a List of Cards and returns true if the Cards have two pairs.

--- src/main/java/pokerGame/Hands.java
+++ src/main/java/pokerGame/Hands.java
@@ -117,4 +117,34 @@ public class Hands {
 		}
 		return false;
 	}
+	
+	//Check for a Two Pair
+	protected static Boolean isTwoPair(List<Card> hand) {
+		int pair1 = 1;
+		int pair2 = 1;
+		int changed = 0;
+		String rank = hand.get(0).getRank();
+		for(int i=1; i<hand.size(); i++) {
+			if(hand.get(i).getRank().equals(rank)) {
+				if(changed == 0) {
+					pair1 += 1;
+				} else if(changed == 1 && pair1 == 1) {
+					pair1 += 1;
+				} else if(changed == 1 && pair1 == 2) {
+					pair2 += 1;
+				} else if (changed == 2 && pair2 == 1) {
+					pair2 += 1;
+				} else if (changed == 2 && pair2 == 2) {
+					return false;
+				}
+			} else {
+				rank = hand.get(i).getRank();
+				changed += 1;
+			}
+		}
+		if((pair1 == 2 && pair2 == 2)) {
+			return true;
+		}
+		return false;
+	}
 }
