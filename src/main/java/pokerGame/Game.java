fb724a700b66d4ebfe2b18a2b4f65726f28175c5
Walker <walker_matt@hotmail.ca>
2018-09-26 11:00:27 -0400

    (Code) Updated Game
    Updated game to meet the new requirements of TestGame after it had been refactored a couple times.

--- src/main/java/pokerGame/Game.java
+++ src/main/java/pokerGame/Game.java
@@ -4,10 +4,8 @@ import java.util.*;
 
 public class Game {
 	static Deck deck;
-	static List<Card> river;
 	static List<Card> cardsToBeat;
 	static List<Card> AIPcards;
-	static String riverDisplay;
 	static String cardsToBeatDisplay;
 	static String AIPcardsDisplay;
 	
@@ -15,28 +13,30 @@ public class Game {
 		deck = new Deck();
 		deck.shuffle();
 		
-		river = new ArrayList<Card>();
-		river.addAll(deck.draw(3));
-		
 		cardsToBeat = new ArrayList<Card>();
-		cardsToBeat.addAll(deck.draw(2));
+		cardsToBeat.addAll(deck.draw(5));
 		
 		AIPcards = new ArrayList<Card>();
-		AIPcards.addAll(deck.draw(2));
+		AIPcards.addAll(deck.draw(5));
 		
-		riverDisplay = "RIVER: " + river.get(0).toString() + " " +
-				river.get(1).toString() + " " +
-				river.get(2).toString() + "\n";
-		cardsToBeatDisplay = "Cards to beat: " + cardsToBeat.get(0).toString() + 
-				" " + cardsToBeat.get(1).toString() + "\n";
-		AIPcardsDisplay = "AIP cards: " + AIPcards.get(0).toString() + " " +
-		AIPcards.get(1).toString() + "\n";
+		cardsToBeatDisplay = "Cards to beat: " + 
+				cardsToBeat.get(0).toString() + " " + 
+				cardsToBeat.get(1).toString() + " " + 
+				cardsToBeat.get(2).toString() + " " + 
+				cardsToBeat.get(3).toString() + " " + 
+				cardsToBeat.get(4).toString() + "\n";
+		
+		AIPcardsDisplay = "AIP cards: " + 
+				AIPcards.get(0).toString() + " " +
+				AIPcards.get(1).toString() + " " +
+				AIPcards.get(2).toString() + " " +
+				AIPcards.get(3).toString() + " " +
+				AIPcards.get(4).toString() + "\n";
 		
 		display();
 	}
 
 	public static void display() {
-		System.out.print(riverDisplay);
 		System.out.print(cardsToBeatDisplay);
 		System.out.print(AIPcardsDisplay);
 	}
