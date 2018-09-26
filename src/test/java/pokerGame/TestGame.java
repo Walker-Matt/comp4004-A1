616c23d2aa6e410385f30f0199bdac08b0c1533b
Walker <walker_matt@hotmail.ca>
2018-09-26 10:57:01 -0400

    (Refactor) Fixed TestGame
    Fixed TestGame tests, since the 'river' variable no longer exists, and the size of the hands have been adusted accordingly.

--- src/test/java/pokerGame/TestGame.java
+++ src/test/java/pokerGame/TestGame.java
@@ -17,13 +17,13 @@ public class TestGame extends TestCase {
 		
 		assertTrue(Game.cardsToBeat instanceof List<?>);
 		assertTrue(Game.cardsToBeat.get(0) instanceof Card);
-		assertEquals(2, Game.cardsToBeat.size());
+		assertEquals(5, Game.cardsToBeat.size());
 		
 		assertTrue(Game.AIPcards instanceof List<?>);
 		assertTrue(Game.AIPcards.get(0) instanceof Card);
-		assertEquals(2, Game.AIPcards.size());
+		assertEquals(5, Game.AIPcards.size());
 		
-		assertEquals(45, Game.deck.getSize());
+		assertEquals(42, Game.deck.getSize());
 	}
 	
 	//test String values used for display
@@ -31,10 +31,18 @@ public class TestGame extends TestCase {
 		String[] args = new String[0];
 		Game.main(args);
 		
-		String cardsToBeat = "Cards to beat: " + Game.cardsToBeat.get(0).toString() +
-				" " + Game.cardsToBeat.get(1).toString() + "\n";
-		String AIPcards = "AIP cards: " + Game.AIPcards.get(0).toString() + " " +
-				Game.AIPcards.get(1).toString()+ "\n";
+		String cardsToBeat = "Cards to beat: " + 
+				Game.cardsToBeat.get(0).toString() + " " +
+				Game.cardsToBeat.get(1).toString() + " " +
+				Game.cardsToBeat.get(2).toString() + " " +
+				Game.cardsToBeat.get(3).toString() + " " +
+				Game.cardsToBeat.get(4).toString() + "\n";
+		String AIPcards = "AIP cards: " + 
+				Game.AIPcards.get(0).toString() + " " +
+				Game.AIPcards.get(1).toString() + " " +
+				Game.AIPcards.get(2).toString() + " " +
+				Game.AIPcards.get(3).toString() + " " +
+				Game.AIPcards.get(4).toString()+ "\n";
 		
 		assertEquals(cardsToBeat, Game.cardsToBeatDisplay);
 		assertEquals(AIPcards, Game.AIPcardsDisplay);
