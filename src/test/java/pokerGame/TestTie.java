f6c71b41c942da5b4a2a32b2d343056191821465
Walker <walker_matt@hotmail.ca>
2018-09-26 19:51:54 -0400

    (Test) Added test to TestTie
    Added a test for the settlePair method that will settle a tie between two pair hands.

--- src/test/java/pokerGame/TestTie.java
+++ src/test/java/pokerGame/TestTie.java
@@ -10,6 +10,25 @@ public class TestTie extends TestCase {
 		super(name);
 	}
 	
+	//test settlePair() method
+	public void testTieSettlePair() {
+		List<Card> cardsToBeat = new ArrayList<Card>();
+		cardsToBeat.add(new Card("S", "4"));
+		cardsToBeat.add(new Card("H", "J"));
+		cardsToBeat.add(new Card("C", "J"));
+		cardsToBeat.add(new Card("D", "7"));
+		cardsToBeat.add(new Card("S", "3"));
+		
+		List<Card> AIPcards = new ArrayList<Card>();
+		AIPcards.add(new Card("S", "4"));
+		AIPcards.add(new Card("H", "5"));
+		AIPcards.add(new Card("C", "5"));
+		AIPcards.add(new Card("D", "7"));
+		AIPcards.add(new Card("S", "3"));
+		
+		assertEquals("AIP loses.", Tie.settleHighCard(cardsToBeat, AIPcards));
+	}
+	
 	//test settleHighCard() method
 	public void testTieSettleHighCard() {
 		List<Card> cardsToBeat = new ArrayList<Card>();
