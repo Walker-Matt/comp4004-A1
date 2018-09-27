b499732e53e440cf9a46b45b0f416083489fd68b
Walker <walker_matt@hotmail.ca>
2018-09-26 22:37:52 -0400

    (Code) Added methods to Tie
    Added method that returns the highest pair card from a two pair hand. Added method that settles a tie between two hands that are both Two Pair.

--- src/main/java/pokerGame/Tie.java
+++ src/main/java/pokerGame/Tie.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class Tie {
 	
+	protected static String settleTwoPair(List<Card> cardsToBeat, List<Card> AIPcards) {
+		if(twoPair(cardsToBeat).getOrder() > twoPair(AIPcards).getOrder()) {
+			return "AIP loses.";
+		} else if (twoPair(cardsToBeat).getOrder() < twoPair(AIPcards).getOrder()) {
+			return "AIP wins.";
+		} else {
+			if(suitScore(twoPair(cardsToBeat)) > suitScore(twoPair(AIPcards))) {
+				return "AIP loses.";
+			} else {
+				return "AIP wins.";
+			}
+		}
+	}
+	
 	protected static String settlePair(List<Card> cardsToBeat, List<Card> AIPcards) {
 		if(pair(cardsToBeat).getOrder() > pair(AIPcards).getOrder()) {
 			return "AIP loses.";
@@ -29,6 +43,31 @@ public class Tie {
 			} else {
 				return "AIP wins.";
 			}
+		}
+	}
+	
+	protected static Card twoPair(List<Card> hand) {
+		Card firstPair = hand.get(0);
+		for(int i=1; i < hand.size(); i++) {
+			if(hand.get(i).getRank() == firstPair.getRank()) {
+				break;
+			} else {
+				firstPair = hand.get(i);
+			}
+		}
+		Card secondPair = hand.get(0);
+		for(int i=1; i < hand.size(); i++) {
+			if(hand.get(i).getRank() == secondPair.getRank() &&
+				hand.get(i).getRank() != firstPair.getRank()) {
+				break;
+			} else {
+				secondPair = hand.get(i);
+			}
+		}
+		if(firstPair.getOrder() > secondPair.getOrder()) {
+			return firstPair;
+		} else {
+			return secondPair;
 		}
 	}
 	
