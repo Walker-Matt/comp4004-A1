dcb0e35fa71af07aa4f6ef706b1b4a603699eb64
Walker <walker_matt@hotmail.ca>
2018-09-26 22:03:19 -0400

    (Test) Added test to TestTie
    Added a test for a method that will take a hand with two pairs, and return the Card value of the highest pair. Used for settling ties.

--- src/test/java/pokerGame/TestTie.java
+++ src/test/java/pokerGame/TestTie.java
@@ -67,6 +67,18 @@ public class TestTie extends TestCase {
 		assertEquals("AIP loses.", Tie.settleHighCard(cardsToBeat, AIPcards));
 	}
 	
+	//test twoPair() method
+	public void testTieTwoPair() {
+		List<Card> twoPair = new ArrayList<Card>();
+		twoPair.add(new Card("S", "4"));
+		twoPair.add(new Card("H", "4"));
+		twoPair.add(new Card("C", "J"));
+		twoPair.add(new Card("D", "J"));
+		twoPair.add(new Card("S", "3"));
+		
+		assertEquals(10, Tie.twoPair(twoPair).getOrder());
+	}
+	
 	//test pair() method
 	public void testTiePair() {
 		List<Card> pair = new ArrayList<Card>();
