148fe646ab99b1eceed61120b5fe8a0c8d3dfd04
Walker <walker_matt@hotmail.ca>
2018-09-26 23:24:09 -0400

    (Test) Added test to TestTie
    Added test for a method that will settle a tie between two Three of a Kind hands.

--- src/test/java/pokerGame/TestTie.java
+++ src/test/java/pokerGame/TestTie.java
@@ -48,6 +48,25 @@ public class TestTie extends TestCase {
 		assertEquals("AIP loses.", Tie.settleHighCard(cardsToBeat, AIPcards));
 	}
 	
+	//test settleThreeOfAKind method
+	public void testTieSettleThreeOfAKind() {
+		List<Card> cardsToBeat = new ArrayList<Card>();
+		cardsToBeat.add(new Card("S", "4"));
+		cardsToBeat.add(new Card("H", "J"));
+		cardsToBeat.add(new Card("C", "J"));
+		cardsToBeat.add(new Card("D", "J"));
+		cardsToBeat.add(new Card("S", "3"));
+		
+		List<Card> AIPcards = new ArrayList<Card>();
+		AIPcards.add(new Card("S", "4"));
+		AIPcards.add(new Card("H", "4"));
+		AIPcards.add(new Card("C", "4"));
+		AIPcards.add(new Card("D", "7"));
+		AIPcards.add(new Card("S", "3"));
+		
+		assertEquals("AIP loses.", Tie.settleThreeOfAKind(cardsToBeat, AIPcards));
+	}
+	
 	//test settleTwoPair method
 	public void testTieSettleTwoPair() {
 		List<Card> cardsToBeat = new ArrayList<Card>();
