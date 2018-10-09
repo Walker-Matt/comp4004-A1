7847b050bfd32500ff49fa4349bacdbe88da09a7
Walker <walker_matt@hotmail.ca>
2018-10-09 19:13:10 -0400

    (Refactor) Removed hardcoding from TestFourOfAKindTie

--- src/test/java/pokerGame/TestFourOfAKindTie.java
+++ src/test/java/pokerGame/TestFourOfAKindTie.java
@@ -9,22 +9,14 @@ public class TestFourOfAKindTie extends TestCase {
 	public TestFourOfAKindTie(String name) {
 		super(name);
 	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_four_of_a_kind_tie.txt");
 
 	//tests two four of a kind hands
 	public void testTie() {
-		List<Card> FourOfAKind1 = new ArrayList<Card>();
-		FourOfAKind1.add(new Card("S", "4"));
-		FourOfAKind1.add(new Card("H", "4"));
-		FourOfAKind1.add(new Card("C", "4"));
-		FourOfAKind1.add(new Card("D", "4"));
-		FourOfAKind1.add(new Card("S", "2"));
+		List<Card> FourOfAKind1 = hands.get(0).getDeck();
 		
-		List<Card> FourOfAKind2 = new ArrayList<Card>();
-		FourOfAKind2.add(new Card("S", "5"));
-		FourOfAKind2.add(new Card("H", "5"));
-		FourOfAKind2.add(new Card("C", "5"));
-		FourOfAKind2.add(new Card("D", "5"));
-		FourOfAKind2.add(new Card("S", "3"));
+		List<Card> FourOfAKind2 = hands.get(1).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(8, FourOfAKind1, FourOfAKind2));
 	}
