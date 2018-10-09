17c7b339e49d1f3603d6ae037204dcd47147073a
Walker <walker_matt@hotmail.ca>
2018-10-09 18:13:35 -0400

    (Refactor) Removed hardcoding from TestStraight

--- src/test/java/pokerGame/TestStraight.java
+++ src/test/java/pokerGame/TestStraight.java
@@ -9,16 +9,13 @@ public class TestStraight extends TestCase {
 	public TestStraight(String name) {
 		super(name);
 	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_straight.txt");
 
 	//test average straight hand
 	//test method determining a straight hand
 	public void testStraight() {
-		List<Card> Straight = new ArrayList<Card>();
-		Straight.add(new Card("S", "4"));
-		Straight.add(new Card("H", "5"));
-		Straight.add(new Card("C", "6"));
-		Straight.add(new Card("D", "7"));
-		Straight.add(new Card("S", "8"));
+		List<Card> Straight = hands.get(0).getDeck();
 		assertTrue(Hands.isStraight(Straight));
 		assertFalse(Hands.isRoyalFlush(Straight));
 		assertFalse(Hands.isStraightFlush(Straight));
@@ -32,12 +29,7 @@ public class TestStraight extends TestCase {
 	
 	//test input with lowest possible straight
 	public void testLowest() {
-		List<Card> Straight = new ArrayList<Card>();
-		Straight.add(new Card("S", "2"));
-		Straight.add(new Card("H", "3"));
-		Straight.add(new Card("C", "4"));
-		Straight.add(new Card("D", "5"));
-		Straight.add(new Card("S", "6"));
+		List<Card> Straight = hands.get(1).getDeck();
 		assertTrue(Hands.isStraight(Straight));
 		assertFalse(Hands.isRoyalFlush(Straight));
 		assertFalse(Hands.isStraightFlush(Straight));
@@ -51,12 +43,7 @@ public class TestStraight extends TestCase {
 	
 	//test input with highest possible straight
 	public void testHighest() {
-		List<Card> Straight = new ArrayList<Card>();
-		Straight.add(new Card("S", "10"));
-		Straight.add(new Card("H", "J"));
-		Straight.add(new Card("C", "Q"));
-		Straight.add(new Card("D", "K"));
-		Straight.add(new Card("S", "A"));
+		List<Card> Straight = hands.get(2).getDeck();
 		assertTrue(Hands.isStraight(Straight));
 		assertFalse(Hands.isRoyalFlush(Straight));
 		assertFalse(Hands.isStraightFlush(Straight));
