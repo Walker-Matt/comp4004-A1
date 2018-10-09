2795528b3295d3d4788beba0cb64bec769012dea
Walker <walker_matt@hotmail.ca>
2018-10-09 19:15:25 -0400

    (Refactor) Removed hardcoding from TestStraightFlushTie

--- src/test/java/pokerGame/TestStraightFlushTie.java
+++ src/test/java/pokerGame/TestStraightFlushTie.java
@@ -9,41 +9,23 @@ public class TestStraightFlushTie extends TestCase {
 	public TestStraightFlushTie(String name) {
 		super(name);
 	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_straight_flush_tie.txt");
 
 	//tests two straight flush hands with different high cards
 	public void testDifferentHighCards() {
-		List<Card> straightFlush1 = new ArrayList<Card>();
-		straightFlush1.add(new Card("H", "4"));
-		straightFlush1.add(new Card("H", "5"));
-		straightFlush1.add(new Card("H", "6"));
-		straightFlush1.add(new Card("H", "7"));
-		straightFlush1.add(new Card("H", "8"));
+		List<Card> straightFlush1 = hands.get(0).getDeck();
 		
-		List<Card> straightFlush2 = new ArrayList<Card>();
-		straightFlush2.add(new Card("H", "5"));
-		straightFlush2.add(new Card("H", "6"));
-		straightFlush2.add(new Card("H", "7"));
-		straightFlush2.add(new Card("H", "8"));
-		straightFlush2.add(new Card("H", "9"));
+		List<Card> straightFlush2 = hands.get(1).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(9, straightFlush1, straightFlush2));
 	}
 	
 	//tests two straight flush hands with same high cards
 	public void testSameHighCards() {
-		List<Card> straightFlush1 = new ArrayList<Card>();
-		straightFlush1.add(new Card("H", "4"));
-		straightFlush1.add(new Card("H", "5"));
-		straightFlush1.add(new Card("H", "6"));
-		straightFlush1.add(new Card("H", "7"));
-		straightFlush1.add(new Card("H", "8"));
+		List<Card> straightFlush1 = hands.get(2).getDeck();
 		
-		List<Card> straightFlush2 = new ArrayList<Card>();
-		straightFlush2.add(new Card("S", "4"));
-		straightFlush2.add(new Card("S", "5"));
-		straightFlush2.add(new Card("S", "6"));
-		straightFlush2.add(new Card("S", "7"));
-		straightFlush2.add(new Card("S", "8"));
+		List<Card> straightFlush2 = hands.get(3).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(9, straightFlush1, straightFlush2));
 	}
