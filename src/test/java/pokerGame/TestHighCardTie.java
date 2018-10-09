2788fd534106e0e67c7caadf20e3a1f620d8d5b0
Walker <walker_matt@hotmail.ca>
2018-10-09 18:54:16 -0400

    (Refactor) Removed hardcoding from TestHighCardTie

--- src/test/java/pokerGame/TestHighCardTie.java
+++ src/test/java/pokerGame/TestHighCardTie.java
@@ -9,22 +9,14 @@ public class TestHighCardTie extends TestCase {
 	public TestHighCardTie(String name) {
 		super(name);
 	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_high_card_tie.txt");
 
 	//tests two high card hands with same high card
 	public void testTie() {
-		List<Card> HighCard1 = new ArrayList<Card>();
-		HighCard1.add(new Card("S", "4"));
-		HighCard1.add(new Card("H", "J"));
-		HighCard1.add(new Card("C", "9"));
-		HighCard1.add(new Card("D", "7"));
-		HighCard1.add(new Card("S", "3"));
+		List<Card> HighCard1 = hands.get(0).getDeck();
 		
-		List<Card> HighCard2 = new ArrayList<Card>();
-		HighCard2.add(new Card("S", "4"));
-		HighCard2.add(new Card("S", "J"));
-		HighCard2.add(new Card("C", "9"));
-		HighCard2.add(new Card("D", "7"));
-		HighCard2.add(new Card("S", "3"));
+		List<Card> HighCard2 = hands.get(1).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(1, HighCard1, HighCard2));
 	}
