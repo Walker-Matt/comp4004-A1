bac3cab46a123e573f54d97b5a14e77c1a1082b7
Walker <walker_matt@hotmail.ca>
2018-10-09 12:17:52 -0400

    (Test) Added a test to TestReadFile
    Added a test for input with multiple lines, and therefor multiple games.

--- src/test/java/pokerGame/TestReadFile.java
+++ src/test/java/pokerGame/TestReadFile.java
@@ -15,4 +15,15 @@ public class TestReadFile extends TestCase {
 		assertEquals(1, deck.size());
 		assertEquals(52, deck.get(0).getSize());
 	}
+	
+	//test for file with more than one game
+	public void testReadMultiple() {
+		List<Deck> deck = new ArrayList<Deck>();
+		deck = ReadFile.read("cards/game_of_suits.txt");
+		assertEquals(4, deck.size());
+		assertEquals(13, deck.get(0).getSize());
+		assertEquals(13, deck.get(1).getSize());
+		assertEquals(13, deck.get(2).getSize());
+		assertEquals(13, deck.get(3).getSize());
+	}
 }
