7184cd7501433df0c789742472dbcce333365f95
Walker <walker_matt@hotmail.ca>
2018-10-09 19:04:21 -0400

    (Refactor) Removed hardcoding from TestStraightTie

--- src/test/java/pokerGame/TestStraightTie.java
+++ src/test/java/pokerGame/TestStraightTie.java
@@ -10,21 +10,13 @@ public class TestStraightTie extends TestCase {
 		super(name);
 	}
 	
+	List<Deck> hands = ReadFile.read("tests/test_straight_tie.txt");
+	
 	//tests two straight hands with different high cards
 	public void testDifferentHighCards() {
-		List<Card> Straight1 = new ArrayList<Card>();
-		Straight1.add(new Card("S", "4"));
-		Straight1.add(new Card("H", "5"));
-		Straight1.add(new Card("C", "6"));
-		Straight1.add(new Card("D", "7"));
-		Straight1.add(new Card("S", "8"));
+		List<Card> Straight1 = hands.get(0).getDeck();
 		
-		List<Card> Straight2 = new ArrayList<Card>();
-		Straight2.add(new Card("S", "5"));
-		Straight2.add(new Card("H", "6"));
-		Straight2.add(new Card("C", "7"));
-		Straight2.add(new Card("D", "8"));
-		Straight2.add(new Card("S", "9"));
+		List<Card> Straight2 = hands.get(1).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(5, Straight1, Straight2));
 	}
@@ -32,19 +24,9 @@ public class TestStraightTie extends TestCase {
 	
 	//tests two straight hands with same high cards
 	public void testSameHighCards() {
-		List<Card> Straight1 = new ArrayList<Card>();
-		Straight1.add(new Card("H", "4"));
-		Straight1.add(new Card("C", "5"));
-		Straight1.add(new Card("D", "6"));
-		Straight1.add(new Card("S", "7"));
-		Straight1.add(new Card("H", "8"));
+		List<Card> Straight1 = hands.get(2).getDeck();
 		
-		List<Card> Straight2 = new ArrayList<Card>();
-		Straight2.add(new Card("S", "4"));
-		Straight2.add(new Card("H", "5"));
-		Straight2.add(new Card("C", "6"));
-		Straight2.add(new Card("D", "7"));
-		Straight2.add(new Card("S", "8"));
+		List<Card> Straight2 = hands.get(3).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(5, Straight1, Straight2));
 	}
