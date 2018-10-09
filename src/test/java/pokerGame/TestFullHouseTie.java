46e25b7bb96e394c424d4ac74eef93feee098ed6
Walker <walker_matt@hotmail.ca>
2018-10-09 19:11:28 -0400

    (Refactor) Removed hardcoding from TestFullHouseTie

--- src/test/java/pokerGame/TestFullHouseTie.java
+++ src/test/java/pokerGame/TestFullHouseTie.java
@@ -9,22 +9,14 @@ public class TestFullHouseTie extends TestCase {
 	public TestFullHouseTie(String name) {
 		super(name);
 	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_full_house_tie.txt");
 
 	//tests two full house hands
 	public void testTie() {
-		List<Card> FullHouse1 = new ArrayList<Card>();
-		FullHouse1.add(new Card("S", "4"));
-		FullHouse1.add(new Card("H", "4"));
-		FullHouse1.add(new Card("C", "4"));
-		FullHouse1.add(new Card("D", "7"));
-		FullHouse1.add(new Card("S", "7"));
+		List<Card> FullHouse1 = hands.get(0).getDeck();
 		
-		List<Card> FullHouse2 = new ArrayList<Card>();
-		FullHouse2.add(new Card("S", "5"));
-		FullHouse2.add(new Card("H", "5"));
-		FullHouse2.add(new Card("C", "5"));
-		FullHouse2.add(new Card("D", "6"));
-		FullHouse2.add(new Card("S", "6"));
+		List<Card> FullHouse2 = hands.get(1).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(7, FullHouse1, FullHouse2));
 	}
