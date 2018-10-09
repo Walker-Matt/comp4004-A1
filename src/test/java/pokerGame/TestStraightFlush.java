ea481dd2c74ce546d79c8625f3213901451174d2
Walker <walker_matt@hotmail.ca>
2018-10-09 18:23:30 -0400

    (Refactor) Removed hardcoding from TestStraightFlush

--- src/test/java/pokerGame/TestStraightFlush.java
+++ src/test/java/pokerGame/TestStraightFlush.java
@@ -9,16 +9,13 @@ public class TestStraightFlush extends TestCase {
 	public TestStraightFlush(String name) {
 		super(name);
 	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_straight_flush.txt");
 
 	//test average straight flush hand
 	//test method determining a straight flush hand
 	public void testStraightFlush() {
-		List<Card> straightFlush = new ArrayList<Card>();
-		straightFlush.add(new Card("H", "4"));
-		straightFlush.add(new Card("H", "5"));
-		straightFlush.add(new Card("H", "6"));
-		straightFlush.add(new Card("H", "7"));
-		straightFlush.add(new Card("H", "8"));
+		List<Card> straightFlush = hands.get(0).getDeck();
 		assertTrue(Hands.isStraightFlush(straightFlush));
 		assertFalse(Hands.isRoyalFlush(straightFlush));
 		assertFalse(Hands.isFourOfAKind(straightFlush));
@@ -30,12 +27,7 @@ public class TestStraightFlush extends TestCase {
 	
 	//test input with lowest possible straight flush
 	public void testLowest() {
-		List<Card> straightFlush = new ArrayList<Card>();
-		straightFlush.add(new Card("C", "2"));
-		straightFlush.add(new Card("C", "3"));
-		straightFlush.add(new Card("C", "4"));
-		straightFlush.add(new Card("C", "5"));
-		straightFlush.add(new Card("C", "6"));
+		List<Card> straightFlush = hands.get(1).getDeck();
 		assertTrue(Hands.isStraightFlush(straightFlush));
 		assertFalse(Hands.isRoyalFlush(straightFlush));
 		assertFalse(Hands.isFourOfAKind(straightFlush));
@@ -47,12 +39,7 @@ public class TestStraightFlush extends TestCase {
 	
 	//test input with highest possible straight flush
 	public void testHighest() {
-		List<Card> straightFlush = new ArrayList<Card>();
-		straightFlush.add(new Card("S", "9"));
-		straightFlush.add(new Card("S", "10"));
-		straightFlush.add(new Card("S", "J"));
-		straightFlush.add(new Card("S", "Q"));
-		straightFlush.add(new Card("S", "K"));
+		List<Card> straightFlush = hands.get(2).getDeck();
 		assertTrue(Hands.isStraightFlush(straightFlush));
 		assertFalse(Hands.isRoyalFlush(straightFlush));
 		assertFalse(Hands.isFourOfAKind(straightFlush));
