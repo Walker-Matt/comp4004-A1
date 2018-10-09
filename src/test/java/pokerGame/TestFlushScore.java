c8b891aa9e13acd61e6d5bdfba70f5457a4fdf2a
Walker <walker_matt@hotmail.ca>
2018-10-09 14:06:44 -0400

    (Test) Score-16 test case
    Added a new test case TestFlushScore.

---
+++ src/test/java/pokerGame/TestFlushScore.java
@@ -0,0 +1,183 @@
+package pokerGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.*;
+
+public class TestFlushScore extends TestCase {
+	public TestFlushScore(String name) {
+		super(name);
+	}
+
+	//tests a flush vs a royal flush
+	public void testVsRoyalFlush() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> royalFlush = new ArrayList<Card>();
+		royalFlush.add(new Card("H", "10"));
+		royalFlush.add(new Card("H", "J"));
+		royalFlush.add(new Card("H", "Q"));
+		royalFlush.add(new Card("H", "K"));
+		royalFlush.add(new Card("H", "A"));
+		
+		assertTrue(Hands.type(Flush) < Hands.type(royalFlush));
+	}
+	
+	//tests a flush vs a straight flush
+	public void testVsStraightFlush() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> straightFlush = new ArrayList<Card>();
+		straightFlush.add(new Card("H", "4"));
+		straightFlush.add(new Card("H", "5"));
+		straightFlush.add(new Card("H", "6"));
+		straightFlush.add(new Card("H", "7"));
+		straightFlush.add(new Card("H", "8"));
+		
+		assertTrue(Hands.type(Flush) < Hands.type(straightFlush));
+	}
+	
+	//tests a flush vs a four of a kind
+	public void testVsFourOfAKind() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> FourOfAKind = new ArrayList<Card>();
+		FourOfAKind.add(new Card("S", "4"));
+		FourOfAKind.add(new Card("H", "4"));
+		FourOfAKind.add(new Card("C", "4"));
+		FourOfAKind.add(new Card("D", "4"));
+		FourOfAKind.add(new Card("S", "3"));
+		
+		assertTrue(Hands.type(Flush) < Hands.type(FourOfAKind));
+	}
+	
+	//tests a flush vs a full house
+	public void testVsFullHouse() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> FullHouse = new ArrayList<Card>();
+		FullHouse.add(new Card("S", "4"));
+		FullHouse.add(new Card("H", "4"));
+		FullHouse.add(new Card("C", "4"));
+		FullHouse.add(new Card("D", "7"));
+		FullHouse.add(new Card("S", "7"));
+		
+		assertTrue(Hands.type(Flush) < Hands.type(FullHouse));
+	}
+	
+	//tests a flush vs a straight
+	public void testVsStraight() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> Straight = new ArrayList<Card>();
+		Straight.add(new Card("S", "4"));
+		Straight.add(new Card("H", "5"));
+		Straight.add(new Card("C", "6"));
+		Straight.add(new Card("D", "7"));
+		Straight.add(new Card("S", "8"));
+		
+		assertTrue(Hands.type(Flush) > Hands.type(Straight));
+	}
+	
+	//tests a flush vs a three of a kind
+	public void testVsThreeOfAKind() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> ThreeOfAKind = new ArrayList<Card>();
+		ThreeOfAKind.add(new Card("S", "4"));
+		ThreeOfAKind.add(new Card("H", "4"));
+		ThreeOfAKind.add(new Card("C", "4"));
+		ThreeOfAKind.add(new Card("D", "7"));
+		ThreeOfAKind.add(new Card("S", "3"));
+		
+		assertTrue(Hands.type(Flush) > Hands.type(ThreeOfAKind));
+	}
+	
+	//tests a flush vs a two pair
+	public void testVsTwoPair() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> TwoPair = new ArrayList<Card>();
+		TwoPair.add(new Card("S", "4"));
+		TwoPair.add(new Card("H", "4"));
+		TwoPair.add(new Card("C", "9"));
+		TwoPair.add(new Card("D", "9"));
+		TwoPair.add(new Card("S", "3"));
+		
+		assertTrue(Hands.type(Flush) > Hands.type(TwoPair));
+	}
+	
+	//tests a flush vs a one pair
+	public void testVsOnePair() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> Pair = new ArrayList<Card>();
+		Pair.add(new Card("S", "4"));
+		Pair.add(new Card("H", "4"));
+		Pair.add(new Card("C", "9"));
+		Pair.add(new Card("D", "7"));
+		Pair.add(new Card("S", "3"));
+		
+		assertTrue(Hands.type(Flush) > Hands.type(Pair));
+	}
+	
+	//tests a flush vs a high card
+	public void testVsHighCard() {
+		List<Card> Flush = new ArrayList<Card>();
+		Flush.add(new Card("S", "4"));
+		Flush.add(new Card("S", "J"));
+		Flush.add(new Card("S", "9"));
+		Flush.add(new Card("S", "7"));
+		Flush.add(new Card("S", "3"));
+		
+		List<Card> HighCard = new ArrayList<Card>();
+		HighCard.add(new Card("S", "4"));
+		HighCard.add(new Card("H", "J"));
+		HighCard.add(new Card("C", "9"));
+		HighCard.add(new Card("D", "7"));
+		HighCard.add(new Card("S", "3"));
+		
+		assertTrue(Hands.type(Flush) > Hands.type(HighCard));
+	}
+}
