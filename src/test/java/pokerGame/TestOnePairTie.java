2f5308d035217b6f4d9aa7f4a3b3522ee7157002
Walker <walker_matt@hotmail.ca>
2018-10-09 18:57:01 -0400

    (Refactor) Removed hardcoding from TestOnePairTie

--- src/test/java/pokerGame/TestOnePairTie.java
+++ src/test/java/pokerGame/TestOnePairTie.java
@@ -9,41 +9,23 @@ public class TestOnePairTie extends TestCase {
 	public TestOnePairTie(String name) {
 		super(name);
 	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_one_pair_tie.txt");
 
 	//tests two one pair hands with different pairs
 	public void testDifferentPair() {
-		List<Card> Pair1 = new ArrayList<Card>();
-		Pair1.add(new Card("S", "4"));
-		Pair1.add(new Card("H", "4"));
-		Pair1.add(new Card("C", "9"));
-		Pair1.add(new Card("D", "7"));
-		Pair1.add(new Card("S", "3"));
+		List<Card> Pair1 = hands.get(0).getDeck();
 		
-		List<Card> Pair2 = new ArrayList<Card>();
-		Pair2.add(new Card("S", "5"));
-		Pair2.add(new Card("H", "5"));
-		Pair2.add(new Card("C", "9"));
-		Pair2.add(new Card("D", "7"));
-		Pair2.add(new Card("S", "3"));
+		List<Card> Pair2 = hands.get(1).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(2, Pair1, Pair2));
 	}
 	
 	//tests two one pair hands with same pairs
 	public void testSamePair() {
-		List<Card> Pair1 = new ArrayList<Card>();
-		Pair1.add(new Card("D", "4"));
-		Pair1.add(new Card("C", "4"));
-		Pair1.add(new Card("C", "9"));
-		Pair1.add(new Card("D", "7"));
-		Pair1.add(new Card("S", "3"));
+		List<Card> Pair1 = hands.get(2).getDeck();
 		
-		List<Card> Pair2 = new ArrayList<Card>();
-		Pair2.add(new Card("S", "4"));
-		Pair2.add(new Card("H", "4"));
-		Pair2.add(new Card("C", "9"));
-		Pair2.add(new Card("D", "7"));
-		Pair2.add(new Card("S", "3"));
+		List<Card> Pair2 = hands.get(3).getDeck();
 		
 		assertEquals("AIP wins.", Tie.settle(2, Pair1, Pair2));
 	}
