e6c4c9fd1a8a5c3ec9743faf2d0f581f65583595
Walker <walker_matt@hotmail.ca>
2018-10-10 02:31:34 -0400

    (Test) Strategy-7 test case

---
+++ src/test/java/pokerGame/TestAIPStrategy7.java
@@ -0,0 +1,41 @@
+package pokerGame;
+
+import java.util.List;
+
+import junit.framework.*;
+
+public class TestAIPStrategy7 extends TestCase {
+	public TestAIPStrategy7(String name) {
+		super(name);
+	}
+	
+	List<Deck> hands = ReadFile.read("tests/test_aip_7.txt");
+
+	//tests with two cards the same suit
+	public void testTwo() {
+		List<Card> hand = hands.get(0).getDeck();
+		AIP.exchange(hand);
+		assertEquals(3, AIP.removeIndex.size());
+		assertEquals(0, AIP.removeIndex.get(0).intValue());
+		assertEquals(1, AIP.removeIndex.get(0).intValue());
+		assertEquals(2, AIP.removeIndex.get(0).intValue());
+	}
+	
+	//tests with three cards the same suit
+	public void testThree() {
+		List<Card> hand = hands.get(1).getDeck();
+		AIP.exchange(hand);
+		assertEquals(1, AIP.removeIndex.size());
+		assertEquals(2, AIP.removeIndex.get(0).intValue());
+	}
+	
+	//tests with four cards the same suit
+	public void testFour() {
+		List<Card> hand = hands.get(2).getDeck();
+		AIP.exchange(hand);
+		assertEquals(3, AIP.removeIndex.size());
+		assertEquals(0, AIP.removeIndex.get(0).intValue());
+		assertEquals(1, AIP.removeIndex.get(0).intValue());
+		assertEquals(2, AIP.removeIndex.get(0).intValue());
+	}
+}
